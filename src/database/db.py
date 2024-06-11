edae4e4cd8e99fe2bde04e0e80c12b63ac8976ba
serg <chazovtema@mail.ru>
2024-06-11 11:58:24 +0500

    Changed db attr privacy

--- src/database/db.py
+++ src/database/db.py
@@ -6,11 +6,11 @@ from .models import Base
 
 class DataBase:
     def __init__(self, engine_url: str) -> None:
-        self.engine = create_engine(engine_url)
-        self.sessionmaker = sessionmaker(self.engine, expire_on_commit=False)
+        self._engine = create_engine(engine_url)
+        self._sessionmaker = sessionmaker(self._engine, expire_on_commit=False)
 
     def create(self):
-        Base.metadata.create_all(self.engine)
+        Base.metadata.create_all(self._engine)
 
     def get_session(self):
-        return self.sessionmaker()
+        return self._sessionmaker()
