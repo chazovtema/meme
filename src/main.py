4df6f153650ae1fb1e2e1b2af3fc1d2dd2e4da55
serg <chazovtema@mail.ru>
2024-06-11 11:59:21 +0500

    fixed main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
 import uvicorn
 from app import app_factory
-from api import meme_route_factory
-from services.meme_service import MemeServiceImp
+from api import meme_rt
 
 if __name__=='__main__':
-    meme_rt = meme_route_factory(MemeServiceImp())
     app = app_factory([meme_rt])
     uvicorn.run(app)
